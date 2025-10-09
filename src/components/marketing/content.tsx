a71c3852779d51fe9080c47f98c519fe88d32455
abdout <osmanabdout@hotmail.com>
2025-10-09 14:26:38 +0300

    z

--- src/components/marketing/content.tsx
+++ src/components/marketing/content.tsx
@@ -1,16 +1,16 @@
 
 import Hero from "./hero";
 // import Stack from "./stack";
-// import Testimonial from "./testimonial";
-// import LetsWorkTogether from "./lets-work-together";
+import Testimonial from "./testimonial";
+import LetsWorkTogether from "./lets-work-together";
 // import Wizard from "@/components/wizard";
-// import Time from "./time";
-// import Boost from "./boost";
+import Time from "./time";
+import Boost from "./boost";
 // import OpenSource from "./open-source";
 // import Automated from "@/components/automated/featured";
 // import Codebase from "@/components/landing/codebase";
-// import FAQs from "./faqs";
-// import LogoCloud from "./logo-cloud";
+import FAQs from "./faqs";
+import LogoCloud from "./logo-cloud";
 // import { Gallery } from "@/components/landing/gallery";
 import type { Dictionary } from '@/components/internationalization/dictionaries';
 
@@ -22,18 +22,18 @@ export default function HomeContent({ dictionary }: HomeContentProps) {
   return (
     <main className="flex min-h-screen flex-col">
       <Hero dictionary={dictionary} />
-      {/*<Gallery />*/}
-      {/*<Stack />*/}
-      {/*<Automated />*/}
-      {/*<OpenSource /> */}
-      {/* <Codebase /> */}
-      {/*<Time dictionary={dictionary} />*/}
-      {/* <Wizard /> */}
-      {/*<Testimonial dictionary={dictionary} />*/}
-      {/*<LogoCloud dictionary={dictionary} />*/}
-      {/*<FAQs dictionary={dictionary} />*/}
-      {/*<LetsWorkTogether dictionary={dictionary} />*/}
-      {/*<Boost dictionary={dictionary} />*/}
+      {/* <Gallery />
+      <Stack />
+      <Automated />
+      <OpenSource />
+      <Codebase /> */}
+      <Time dictionary={dictionary} />
+      <Wizard />
+      <Testimonial dictionary={dictionary} />
+      <LogoCloud dictionary={dictionary} />
+      <FAQs dictionary={dictionary} />
+      <LetsWorkTogether dictionary={dictionary} />
+      <Boost dictionary={dictionary} />
     </main>
   );
 }
