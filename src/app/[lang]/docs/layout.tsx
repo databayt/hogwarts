83c01c6d10fbf9d5d1c60948a92dc24ea3ed7845
abdout <osmanabdout@hotmail.com>
2025-11-22 11:18:08 +0300

    refactor(docs): replace saas-header with marketing-header

--- src/app/[lang]/docs/layout.tsx
+++ src/app/[lang]/docs/layout.tsx
@@ -1,7 +1,7 @@
 import { source } from "@/lib/source"
 import { DocsSidebar } from "@/components/docs/docs-sidebar"
 import { SidebarProvider } from "@/components/ui/sidebar"
-import SaasHeader from "@/components/template/saas-header/content"
+import MarketingHeader from "@/components/template/marketing-header/content"
 
 interface DocsLayoutProps {
     children: React.ReactNode
@@ -17,7 +17,7 @@ export default async function DocsLayout({ children, params }: DocsLayoutProps) 
             <SidebarProvider className="3xl:fixed:container 3xl:fixed:px-3 min-h-min flex-1 items-start px-0 [--sidebar-width:220px] [--top-spacing:0] lg:grid lg:grid-cols-[var(--sidebar-width)_minmax(0,1fr)] lg:[--sidebar-width:240px] lg:[--top-spacing:calc(var(--spacing)*4)]">
                 <DocsSidebar tree={source.pageTree} lang={lang} />
                 <div className="h-full w-full">
-                    <SaasHeader />
+                    <MarketingHeader />
                     {children}
                 </div>
             </SidebarProvider>
