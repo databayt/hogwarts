cb5606acc81ae5834b4a188382661a65f5be25b9
Osman Abdout <abdout@Osmans-MacBook-Pro.local>
2025-12-02 23:22:37 +0300

    style(time): use blue-500 to match databayt.org testimonial

--- src/components/marketing/time.tsx
+++ src/components/marketing/time.tsx
@@ -10,7 +10,7 @@ const Time = ({ dictionary }: TimeProps) => {
     const timeDict = (dictionary as any).time || { title: 'Time', subtitle: 'We sell the origin of value.' };
 
     return (
-        <section className='flex justify-between items-center py-8 md:py-10 lg:py-12 my-40 bg-blue-600 text-white rounded-md'>
+        <section className='flex justify-between items-center py-8 md:py-10 lg:py-12 my-40 bg-blue-500 text-white rounded-md'>
             <div className='flex flex-col items-start justify-start ps-12'>
                 <h1 className="font-heading font-extrabold text-5xl pt-5 md:text-7xl flex items-center justify-center pb-7">
                     {timeDict.title}
